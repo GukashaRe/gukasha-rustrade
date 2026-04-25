807cd664fc8aee65e983bf4b34d574adda9b03ad
WFGukasha <gukasha@163.com>
2026-04-25 15:45:26 +0800

    unadd git_hash

--- build.rs
+++ build.rs
@@ -46,8 +46,4 @@ fn main() {
         .args(["rev-parse", "HEAD"])
         .output()
         .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-
-    // 注入到环境变量
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
